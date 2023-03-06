13dc995aa6ad79b14c74446ac145e7ffec3a2235
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-03-05 19:15:06 -0500

    Finished IFs, will finish Loops

--- src/main/java/src/main/java/WriteIFs.java
+++ src/main/java/src/main/java/WriteIFs.java
@@ -12,16 +12,23 @@ public class WriteIFs
     public void playerDied(boolean player1) {
         // Write an IF statement that checks “player1.isAlive()” 
         // and if that’s false, calls “displayGameOver(player1)”
-     
+
+        if(isAlive(player1) == false)
+        {
+            displayGameOver(player1);
+     }
     }
     
     public String thermoSTAT(int room) {
         // Write an IF statement that checks the 
         // “temperature(room)” and if that check is less than 70, 
         // calls “heatOn()” else calls “coolOn()”
-
-
-        
+        int cold = tempurature(room);
+        if (cold <70){
+            heatOn();
+        } else {
+            coolOn();
+        }
         return this.ss;
     }
 
@@ -31,13 +38,19 @@ public class WriteIFs
         // AND 
         // “insideTemp()” is less than 62, 
         // calls “startAFire(fireplace1)”
-
+    if (outsideTemp() < 50 || insideTemp() <62)
+    {
+        startAFire(fireplace1);
+    }
     }
 
     public void checkFuel(double fuelLevel) {
         // Write an IF statement that checks “fuelLevel” 
         // and if that check is less than 0.08, calls “refuel()”
-
+    if (fuelLevel < 0.08)
+    {
+        refuel();
+    }
     }
 
 
